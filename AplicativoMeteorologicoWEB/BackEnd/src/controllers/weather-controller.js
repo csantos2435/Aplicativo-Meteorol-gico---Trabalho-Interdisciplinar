857c3f78938af317f6cac970b095b260510a8930
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2023-10-18 13:00:42 -0300

    Update weather-controller.js

--- AplicativoMeteorologicoWEB/BackEnd/src/controllers/weather-controller.js
+++ AplicativoMeteorologicoWEB/BackEnd/src/controllers/weather-controller.js
@@ -21,3 +21,39 @@ const postweather = async (req, res) => {
 }
 
 module.exports = {getweather, postweather};
+
+const express = require('express');
+const WeatherBusiness = require('../business/weather-business');
+
+const router = express.Router();
+const weatherBusiness = new WeatherBusiness();
+
+const getweather = async (req, res) => {
+    const city = req.params.city;
+  try {
+    const weatherData = await weatherBusiness.find(city);
+    if (weatherData) {
+      res.json(weatherData);
+    } else {
+      res.status(404).json({ error: 'Dados climáticos não encontrados.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar dados climáticos.' });
+  }
+};
+
+const postweather = async (req, res) => {
+    const city = req.params.city;
+    try {
+      const weatherData = await weatherBusiness.create(city);
+      if (weatherData) {
+        res.json(weatherData);
+      } else {
+        res.status(404).json({ error: 'Dados climáticos não encontrados.' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao buscar dados climáticos.' });
+    }
+};
+
+module.exports = {getweather, postweather};
