90050f9b1a6a25f34a6cd2ebe11c3f14709afbb3
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2023-10-18 13:00:21 -0300

    Update weather-business.js

--- AplicativoMeteorologicoWEB/BackEnd/src/business/weather-business.js
+++ AplicativoMeteorologicoWEB/BackEnd/src/business/weather-business.js
@@ -10,3 +10,34 @@ const find = async (weather) => {
 }
 
 module.exports = { create, find };
+
+const axios = require('axios');
+const weatherRepository = require('../repository/weather-repository');
+
+weatherRepository = new WeatherRepository();
+
+const create = async (city) => {
+    try {
+        const apiKey = 'YOUR_API_KEY'; // Substitua pelo seu próprio API Key
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        
+        if (response.data) {
+          const weatherData = response.data;
+          
+          await this.weatherRepository.create(city, weatherData);
+  
+          return weatherData;
+        } else {
+          throw new Error('Não foi possível obter os dados climáticos.');
+        }
+      } catch (error) {
+        throw error;
+      }
+}
+
+const find = async (city) => {
+    const data = await this.weatherRepository.find(city);
+    return data;
+}
+
+module.exports = { create, find };
