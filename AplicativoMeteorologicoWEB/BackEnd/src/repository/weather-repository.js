2f5ba25f78adf158baf96a2026f7da5eda442376
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2023-10-18 13:01:01 -0300

    Update weather-repository.js

--- AplicativoMeteorologicoWEB/BackEnd/src/repository/weather-repository.js
+++ AplicativoMeteorologicoWEB/BackEnd/src/repository/weather-repository.js
@@ -32,3 +32,39 @@ const find = async (filter) => {
 }
 
 module.exports = {create, find};
+
+const axios = require('axios');
+const Weather = require('../schema/weather-schema')
+
+const create = async (city) => {
+
+    try{
+        const apiKey = 'chave';
+        const response = await axios.get("link");
+
+        if (response.data){
+            const weatherData = response.data;
+
+            const saveWeather = new Weather({
+                city: weatherData.name,
+                temperature: weatherData.main.temp,
+                humidity: weatherData.main.humidity,
+                pressure: weatherData.main.pressure
+            });
+            await saveWeather.save();
+            
+            return weatherData;
+        }else{
+            throw new Error('Não foi possivel obter dados climaticos');
+        }
+    }catch(err){
+        throw err;
+    }
+}
+
+const find = async (city) => {
+    const data = await Weather.findOne({ city }).exec();
+    return data;
+}
+
+module.exports = {create, find};
