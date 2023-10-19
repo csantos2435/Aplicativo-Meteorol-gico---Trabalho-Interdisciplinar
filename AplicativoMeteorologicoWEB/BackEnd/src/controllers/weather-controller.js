d87d233f458de66685d4193ac236b49715ffedd6
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2023-10-19 12:04:14 -0300

    Update weather-controller.js

--- AplicativoMeteorologicoWEB/BackEnd/src/controllers/weather-controller.js
+++ AplicativoMeteorologicoWEB/BackEnd/src/controllers/weather-controller.js
@@ -1,27 +1,3 @@
-const weatherBusiness = require('../business/weather-business');
-
-const getweather = async (req, res) => {
-    try{
-        const result = await weatherBusiness.find();
-        return res.send(result).code(200);    
-    }catch (err){
-        console.error(error)
-        res.status(500).send(error)
-    }
-};
-
-const postweather = async (req, res) => {
-    try{
-        const result = await weatherBusiness.create(req.payload);
-        return res.response(result).code(201);
-    }catch(err){
-        console.error(error)
-        res.status(500).send(error)
-    }
-}
-
-module.exports = {getweather, postweather};
-
 const express = require('express');
 const WeatherBusiness = require('../business/weather-business');
 
