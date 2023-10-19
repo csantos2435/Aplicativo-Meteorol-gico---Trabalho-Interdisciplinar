12a8ce8b70203daf6a395e0124a68001074f5c79
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2023-10-19 12:03:30 -0300

    Update weather-business.js

--- AplicativoMeteorologicoWEB/BackEnd/src/business/weather-business.js
+++ AplicativoMeteorologicoWEB/BackEnd/src/business/weather-business.js
@@ -1,16 +1,3 @@
-const weatherRepository = require('../repository/weather-repository');
-
-const create = async (weather) => {
-    return weatherRepository.create(weather);
-}
-
-const find = async (weather) => {
-
-    return weatherRepository.find(weather);
-}
-
-module.exports = { create, find };
-
 const axios = require('axios');
 const weatherRepository = require('../repository/weather-repository');
 
