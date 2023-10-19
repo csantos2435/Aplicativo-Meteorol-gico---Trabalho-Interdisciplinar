5c860b79da45cd1251be90b412c47536cad56834
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2023-10-19 12:07:02 -0300

    Update weather-schema.js

--- AplicativoMeteorologicoWEB/BackEnd/src/schema/weather-schema.js
+++ AplicativoMeteorologicoWEB/BackEnd/src/schema/weather-schema.js
@@ -1,37 +1,14 @@
 const mongoose = require('mongoose');
-const Joi = require('joi');
 
 const weatherSchema = new mongoose.Schema({
-    payload: Joi.object({
-        city: Joi
-            .string()
-            .min(1)
-            .required(),
-        temperature: Joi 
-                .number()
-                .require(),
-        conditions: Joi
-                .String()
-                .require(),
-        humidity: Joi
-                .Number()
-                .require(),
-        pressure: Joi
-                .Number()
-                .require(),
-    }).required()
+   city: String,
+   temperature: Number,
+   conditions: String,
+   humidity: Number,
+   pressure: Number,
 });
 
 const Weather = mongoose.model('Weather', weatherSchema);
 
 module.exports = Weather;
 
-
-
-// const weatherSchema = new mongoose.Schema({
-//   city: String,
-//   temperature: Number,
-//   conditions: String,
-//   humidity: Number,
-//   pressure: Number,
-// });
