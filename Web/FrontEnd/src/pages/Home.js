c706aaf572c11182b9d66867b6067406fe7b606f
Ana Beatriz Ramos <102860356+AnaBeatriz-R@users.noreply.github.com>
2023-11-22 19:58:07 +0000

    FrontEnd-Web

--- Web/FrontEnd/src/pages/Home.js
+++ Web/FrontEnd/src/pages/Home.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { FaMapMarker, FaSun, FaCloud, FaSnowflake, FaUmbrella, FaTint, FaCloudShowersHeavy, FaWind, FaSmog } from 'react-icons/fa';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Bar, BarChart, Pie, PieChart, Cell } from 'recharts';
 import '../Styles/Home.css';
 import { Link } from 'react-router-dom';
 
 
+const data = [
+  { name: 'Janeiro', temperature: 12, umidade: 10, chuva: 5000 },
+  { name: 'Fevereiro', temperature: 15, umidade: 60, chuva: 6000 },
+  { name: 'Março', temperature: 20, umidade: 50, chuva: 7000 },
+  { name: 'Abril', temperature: 25, umidade: 20, chuva: 8000 },
+  { name: 'Maio', temperature: 30, umidade: 30, chuva: 9000 },
+ ];
+ 
+ // Calcular a porcentagem de umidade para cada mês
+ data.forEach(item => {
+  item.umidade = item.umidade / 100;
+ });
+ 
+ const pieColors = [
+  { value: 100, color: '#164863' },
+  { value: 80, color: '#427D9D' },
+  { value: 50, color: '#9BBEC8' },
+  { value: 20, color: '#87C4FF' },
+  { value: 0, color: '#000000' },
+ ];
+
+
 function App() {
-  const cidade = "SuaCidadeAqui";
+  const cidade = "Franca";
   const temperatura = 25;
   const temperaturaAtual = `${temperatura}°C`;
 
@@ -117,7 +140,38 @@ function App() {
               </div>
             </div>
           </div>
-        </div>
+              <div className="graph">
+                <h1 className="graph-title">Temperatura</h1>
+                <LineChart width={500} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                  <Line type="monotone" dataKey="temperature" stroke="#001B79" strokeWidth={6} />
+                  <CartesianGrid stroke="#ccc" strokeDasharray="9 9" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                </LineChart>
+              </div>
+              <div className="graph">   
+              <h1 className="graph-title">Umidade</h1>   
+                <PieChart width={600} height={400}>
+                  <Pie dataKey="umidade" data={data} label={true}>
+                    {
+                      pieColors.map((entry, index) => <Cell key={index} fill={entry.color} />)
+                    }
+                  </Pie>
+                  <Tooltip />
+                </PieChart>
+              </div>
+              <div className="graph last-graph">
+              <h1 className="graph-title">Quantidade de Chuva</h1>
+                <BarChart width={500} height={300} data={data} barSize={50}>
+                  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Bar dataKey="chuva" fill="#001B79" />
+                </BarChart>
+              </div>
+            </div>
   );
 }
 
