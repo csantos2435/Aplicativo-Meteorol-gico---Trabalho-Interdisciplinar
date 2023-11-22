53c23627f2614b837d3fc94a7fd04958c7a0857d
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2023-11-22 18:32:14 -0300

    Update routes.js

--- Web/FrontEnd/src/routes.js
+++ Web/FrontEnd/src/routes.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-=======
 import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
->>>>>>> main
 
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import Pesquisa from './pages/Pesquisa';
-<<<<<<< HEAD
-=======
 import Home from './pages/Home';
->>>>>>> main
 
 
 
@@ -22,10 +15,7 @@ function RoutesApp() {
                 <Route path='/' element={<Login/>}/>
                 <Route path='/Cadastro/:id' element={<Cadastro/>}/>
                 <Route path='/Pesquisa/:id' element={<Pesquisa/>}/>
-<<<<<<< HEAD
-=======
                 <Route path='/Home/:id/:nome' element={<Home/>}/>
->>>>>>> main
             </Routes>
         </BrowserRouter>
     )
